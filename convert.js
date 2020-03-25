f869b3f8023ac615081e2abe8565c9f6dbe1b06f
sayoojtmc <sayoojtmc@gmail.com>
2020-03-25 12:19:53 +0530

    added lowercase api

--- convert.js
+++ convert.js
@@ -1,28 +1,54 @@
 const express = require('express');
 const router = express.Router();
-router.post('/', function(req, res){
-    //Check if all fields are provided and are valid:
-    if(!req.body.json){ 
-       res.status(400);
-       res.json({message: "Bad Request"});
-    } else {
-       var names = req.body.json.names;
-       var address = req.body.json.address;
-       var namesUpper = [];
-       var addressUpper = [];
-       var response = {};
-       names.forEach((value)=>{
+router.post('/convertUpper', function (req, res) {
+   //Check if all fields are provided and are valid:
+   if (!req.body.json) {
+      res.status(400);
+      res.json({ message: "Bad Request" });
+   } else {
+      var names = req.body.json.names;
+      var address = req.body.json.address;
+      var namesUpper = [];
+      var addressUpper = [];
+      var response = {};
+      names.forEach((value) => {
          namesUpper.push(value.toUpperCase());
-       });
-       address.forEach((value)=>{
-          addressUpper.push(value.toUpperCase());
-       });
-       response = {
-          names: namesUpper,
-          address: addressUpper
-       };
-       console.log(response)
-       res.json(response);
-    }
- });
- module.exports = router;
+      });
+      address.forEach((value) => {
+         addressUpper.push(value.toUpperCase());
+      });
+      response = {
+         names: namesUpper,
+         address: addressUpper
+      };
+      console.log(response)
+      res.json(response);
+   }
+});
+router.post('/convertLower', function (req, res) {
+   //Check if all fields are provided and are valid:
+   if (!req.body.json) {
+      res.status(400);
+      res.json({ message: "Bad Request" });
+   } else {
+      var names = req.body.json.names;
+      var address = req.body.json.address;
+      var namesUpper = [];
+      var addressUpper = [];
+      var response = {};
+      names.forEach((value) => {
+         namesUpper.push(value.toLowerCase());
+      });
+      address.forEach((value) => {
+         addressUpper.push(value.toLowerCase());
+      });
+      response = {
+         names: namesUpper,
+         address: addressUpper
+      };
+      console.log(response)
+      res.json(response);
+   }
+});
+
+module.exports = router;
--- express.js
+++ express.js
@@ -3,18 +3,15 @@ var bodyParser = require('body-parser');
 var multer = require('multer');
 var upload = multer();
 var cors = require('cors');
-
 var app = express();
-
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(upload.array());
 
-//Require the Router we defined in movies.js
+//Require the Router we d in movies.js
 var convert = require('./convert.js');
 
-//Use the Router on the sub route /movies
+//Use the Ruter sub route /movies
 app.use('/convert', convert);
-
 app.listen(3000);
